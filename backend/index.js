51a043800fdb29af548b397382db89d55af239dc
Zaryab <zaryabkhan248@gmail.com>
2024-05-06 20:11:08 +0500

    changes

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use((req, res, next) => {
   res.setHeader("Access-Control-Allow-Headers", "Content-Type");
   next();
 });
-app.use("/products", router);
+app.use("/", router);
 // app.get("/items", async (req, res) => {
 //   const storedItems = await getStoredItems();
 //   await new Promise((resolve, reject) => setTimeout(() => resolve(), 2000));
